0cb9631e04cb83f28d75d64e6618b7cd00a90513
VinsDeveloper <137960636+VinsDeveloper@users.noreply.github.com>
2023-07-01 18:24:07 +0700

    Add files via upload

---
+++ server-api.js
@@ -0,0 +1,6 @@
+{
+    "url": "http://kobralocks.com/",
+    "server": "http://kobralocks.com/pirate-server.txt",
+    "server_mining": "http://kobralocks.com/pirate-mining.txt",
+    "server_chat": "http://kobralocks.com/pirate-chat.txt"
+}
